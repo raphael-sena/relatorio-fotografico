1592c865359cb592142237a88c554899e50d5171
raphael sena <rsenares1@gmail.com>
2025-01-23 16:30:17 -0300

    feat: initialize backend with Spring Boot

---
+++ code/backend/inspecao-rei/src/test/java/com/newenergy/inspecao_rei/InspecaoReiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newenergy.inspecao_rei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspecaoReiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
